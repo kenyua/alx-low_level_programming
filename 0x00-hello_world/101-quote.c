14128f0f54411b54cf6cf80c2a12d25192133988
kenyua <kenyuamorris@gmail.com>
2022-12-08 11:01:04 -0800

    yes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
  * Return: one
  */
 int main(void)
-{
-		printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-			return (1);
+{	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
