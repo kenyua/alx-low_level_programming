1acf6822681d515060560246cdafbfa534ae1f60
kenyua <kenyuamorris@gmail.com>
2022-12-08 10:45:11 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 /**
  * main - main block
- * Return: (0);
+ * Return: 0
  */
 int main(void)
 {
@@ -9,6 +10,6 @@ int main(void)
 			printf("Size of an int: %i byte(s)\n", sizeof(int));
 				printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 					printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %i byte(s)\n", sizeof(float);
+						printf("Size of a float: %i byte(s)\n", sizeof(float));
 							return (0);
 }
