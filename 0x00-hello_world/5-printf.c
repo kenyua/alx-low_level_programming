82a577383d015b66e61067e2d67305051e711f40
kenyua <kenyuamorris@gmail.com>
2022-12-08 10:19:42 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- *    * main - Prints onto screen
- *
- *       *
- *
- *          * Return: zero
- *
- *             */
-
+ * main - Prints onto screen
+ * Return: zero
+ */
 int main(void)
-
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-
 			return (0);
-
 }
